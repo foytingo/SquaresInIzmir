af9296e39198b1d159fa7b3f28c4af83fe1559f9
Murat Baykor <foytingo@gmail.com>
2022-10-12 12:46:24 +0300

    Initial Commit

---
+++ SquaresInIzmir/SquaresInIzmirApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquaresInIzmirApp.swift
+//  SquaresInIzmir
+//
+//  Created by Murat Baykor on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SquaresInIzmirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
